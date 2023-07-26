eca730bd88f33429893cddf39d386e2f55840d83
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-07-26 13:55:21 +0330

    add flutter project and work with matrial app and scaffold

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  void onPressed() {
+    print("pressed button");
+  }
+
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: const Text(
+          "I am rich",
+        ),
+        leading: const Center(child: Text('sajjad')),
+        actions: [
+          TextButton(onPressed: onPressed, child: const Text("click me")),
+          TextButton(onPressed: onPressed, child: const Text("click me"))
+        ],
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I665jj0hIYgIjQTiBkFEuXjkOpIPH6nW3ULnCtc&s'),
+        ),
+      ),
+    ),
+  ));
+}
