7b1b3d3c7aff61a91ee89c5e56aa12a55b28f3b9
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-07-29 10:21:25 +0330

    clean main for new app

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  void onPressed() {
-    print("pressed button");
-  }
-
-  runApp(MaterialApp(
-      home: Scaffold(
-    backgroundColor: Colors.blueGrey[900],
-    appBar: AppBar(
-      backgroundColor: Colors.blueGrey[900],
-      title: const Text(
-        "I am rich",
-      ),
-      leading: const Image(
-        image: AssetImage('images/leadIcon.png'),
-      ),
-      actions: [
-        TextButton(onPressed: onPressed, child: const Text("Menu 1")),
-        TextButton(onPressed: onPressed, child: const Text("Menu 2"))
-      ],
-    ),
-    body: const Center(child: Image(image: AssetImage('images/diamond.jpg'))),
-  )));
+  runApp(const MaterialApp(home: Text("Hello world")));
 }
