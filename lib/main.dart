02cd5d8a7292bf8558735d8a66f782f10f165af4
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-07-30 10:17:32 +0330

    work with column and its props

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,34 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Container(
-            color: Colors.blue[100],
-            width: 200,
-            height: 100,
-            // padding: EdgeInsets.all(10),
-            // padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-            padding: const EdgeInsets.fromLTRB(30, 10, 5, 15),
-            // margin: EdgeInsets.all(10),
-            // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-            margin: const EdgeInsets.fromLTRB(30, 10, 5, 15),
-            child: const Text("This is a Container"),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center, // in column x axis => start, center, end, ...
+            verticalDirection: VerticalDirection.down, // up ,default down
+            mainAxisAlignment: MainAxisAlignment.start, // in column y axis => center, end, start, spaceBetween, spaceAround, spaceEvenly
+            children: <Widget>[
+              Container(
+                color: Colors.blue[100],
+                width: 100,
+                height: 100,
+                child: const Text("Container 1"),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                color: Colors.amberAccent[100],
+                width: 150,
+                height: 100,
+                child: const Text("Container 2"),
+              ),
+              Container(
+                color: Colors.deepPurple[100],
+                width: 200,
+                height: 100,
+                child: const Text("Container 3"),
+              ),
+            ],
           ),
         ),
       ),
