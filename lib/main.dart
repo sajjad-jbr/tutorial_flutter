b6240c74c0950b7c8258c34ba11d2e00f5f84931
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-07-30 11:05:03 +0330

    work with row amd its props

--- lib/main.dart
+++ lib/main.dart
@@ -13,36 +13,46 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center, // in column x axis => start, center, end, ...
-            verticalDirection: VerticalDirection.down, // up ,default down
-            mainAxisAlignment: MainAxisAlignment.start, // in column y axis => center, end, start, spaceBetween, spaceAround, spaceEvenly
-            children: <Widget>[
-              Container(
-                color: Colors.blue[100],
-                width: 100,
-                height: 100,
-                child: const Text("Container 1"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                color: Colors.amberAccent[100],
-                width: 150,
-                height: 100,
-                child: const Text("Container 2"),
-              ),
-              Container(
-                color: Colors.deepPurple[100],
-                width: 200,
-                height: 100,
-                child: const Text("Container 3"),
-              ),
-            ],
-          ),
-        ),
+            child: Column(
+            children: [
+            Row(
+              children: [Expanded(child: Container())],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center, // in row y axis => start, center, end, ...
+              verticalDirection: VerticalDirection.down, // up ,default down
+              mainAxisAlignment: MainAxisAlignment.center, // in row x axis => center, end, start, spaceBetween, spaceAround, spaceEvenly
+              children: <Widget>[
+                Container(
+                  color: Colors.blue[100],
+                  width: 100,
+                  height: 100,
+                  child: const Text("Container 1"),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  color: Colors.amberAccent[100],
+                  width: 150,
+                  height: 100,
+                  child: const Text("Container 2"),
+                ),
+                Container(
+                  color: Colors.deepPurple[100],
+                  width: 200,
+                  height: 100,
+                  child: const Text("Container 3"),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                ),
+
+              ],
+            ),
+          ],
+        )),
       ),
     );
   }
