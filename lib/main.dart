dd60c2cd7c0d8a394dee1d6f59f89e8ec10d9376
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-08-02 10:47:23 +0330

    fix visit app and work with ListTile, Divider, Card , ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,44 +8,126 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void onPressed() {}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                width: 100,
-                height: double.infinity,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
+            child: Center(
+          child: Container(
+            width: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/profile.webp'),
+                ),
+                const Text(
+                  "Angela",
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26),
+                ),
+                const Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                      fontFamily: 'Oswald',
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20,
+                      letterSpacing: 2),
+                ),
+                const SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(color: Colors.white,),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ListTile(
+                      leading: const Icon(Icons.phone, color: Colors.teal),
+                      title: Text(
+                        "+98 913 123 4567",
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20,
+                          fontFamily: "Oswald",
+                        ),
+                      ),
+                    ),
                   ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: ListTile(
+                        leading: const Icon(Icons.email, color: Colors.teal),
+                        title: Text(
+                          "abc@email.com",
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20,
+                            fontFamily: "Oswald",
+                          ),
+                        )),
                   ),
-                ],
-              ),
-              Container(
-                width: 100,
-                height: double.infinity,
-                color: Colors.blue,
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
-        ),
+        )),
       ),
     );
   }
 }
+
+/*
+Row(
+children: <Widget>[
+  const Icon(Icons.phone, color: Colors.teal),
+  const SizedBox(
+    width: 10,
+  ),
+  Text(
+    "+98 913 123 4567",
+    style: TextStyle(
+      color: Colors.teal.shade900,
+      fontSize: 20,
+      fontFamily: "Oswald",
+    ),
+  )
+],
+),
+
+
+
+Row(
+children: <Widget>[
+const Icon(Icons.email, color: Colors.teal),
+const SizedBox(
+width: 10,
+),
+Text(
+"abc@email.com",
+style: TextStyle(
+color: Colors.teal.shade900,
+fontSize: 20,
+fontFamily: "Oswald",
+),
+)
+],
+),
+*/
