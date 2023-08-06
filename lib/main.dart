f51943b2c77bff52ce433d4c8f6e26a379e3c5b1
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-08-06 10:15:17 +0330

    solve setp 3 - 8 ball

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('8 Ball magic'),
-        ),
-        body: const Center(child: Text("Hello world!!!"),),
-      ),
+    const MaterialApp(
+      home: BallPage(),
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text("Ask Me Anything"),
+      ),
+      body: Container(
+        child: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset("Assets/images/8balls/ball1.png"),
+    );
+  }
 }
