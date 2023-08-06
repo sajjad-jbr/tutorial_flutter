a7795cc36bfb0329442ce8a4db71e6b4208388fa
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-08-06 09:41:52 +0330

    work with state in flutter and solve dice challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,102 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DiceWidget(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  void onPressed() {}
-
+class DicePage extends StatelessWidget {
+  // const DicePage({super.key});
+  var left = 1;
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Text("hello world"),
+    return Center(
+      child: Container(
+        width: 500,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  left = left + 1;
+                  print("sajjad: $left");
+                },
+                child: Image(
+                  image: AssetImage('Assets/images/dicee/dice$left.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  print("right");
+                },
+                child: const Image(
+                  image: AssetImage('Assets/images/dicee/dice5.png'),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
+class DiceWidget extends StatefulWidget {
+  const DiceWidget({super.key});
+
+  @override
+  State<DiceWidget> createState() => _DiceWidgetState();
+}
+
+class _DiceWidgetState extends State<DiceWidget> {
+  int leftDice = Random().nextInt(6)+1;
+  int rightDice = Random().nextInt(6)+1;
+  void updateBothDice(){
+    setState(() {
+      leftDice = Random().nextInt(6)+1;
+      rightDice = Random().nextInt(6)+1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 500,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: updateBothDice,
+                child: Image(
+                  image: AssetImage('Assets/images/dicee/dice$leftDice.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: updateBothDice,
+                child: Image(
+                  image: AssetImage('Assets/images/dicee/dice$rightDice.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
