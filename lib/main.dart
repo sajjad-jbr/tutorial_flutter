f4be20b99f7e6ed977af51bd74c1b6365f0a6527
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-08-07 11:22:59 +0330

    clear project for zylophone app nad add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,17 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    const MaterialApp(
-      home: BallPage(),
-    ),
-  );
-}
+void main() => runApp(const XylophoneApp());
 
-class BallPage extends StatelessWidget {
-  const BallPage({super.key});
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: const Text("Ask Me Anything"),
-      ),
-      body: Container(
-        child: Ball(),
-      ),
-    );
-  }
-}
-
-class Ball extends StatefulWidget {
-  const Ball({super.key});
-
-  @override
-  State<Ball> createState() => _BallState();
-}
-
-class _BallState extends State<Ball> {
-  int ballNumber = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          setState(() {
-            ballNumber = Random().nextInt(5) + 1;
-          });
-        },
-        child: Image.asset("Assets/images/8balls/ball$ballNumber.png"),
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(child: const Text("hello world")),
+        ),
       ),
     );
   }
