4ee05741836e963d1bb0d7a42ea65d982ddd6419
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-08-09 15:32:40 +0330

    compelete zylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(const XylophoneApp());
 
@@ -7,10 +8,47 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var btnColor = [
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+      Colors.green,
+      Colors.green[900],
+      Colors.blue,
+      Colors.purple
+    ];
+    void handleSound(i) {
+      final player = AudioPlayer();
+      int j = i + 1;
+      player.play(AssetSource("zylophoneSounds/note$j.wav"));
+    }
+    List<Widget> createButton() {
+      List<Widget> res = [];
+      for (int i = 0; i < btnColor.length; i++) {
+        res.add(
+          Expanded(
+            child: Container(
+              color: btnColor[i],
+              child: TextButton(
+                onPressed: () {
+                  handleSound(i);
+                },
+                child: const Text(""),
+              ),
+            ),
+          ),
+        );
+      }
+      return res;
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(child: const Text("hello world")),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: createButton(),
+          ),
         ),
       ),
     );
